2d4fc659056ce5ed2d2123d2052e170afd75cdcf
Marcos <marcos.asensio8@educa.madrid.org>
2025-01-31 08:47:04 +0100

    version 2 con mensaje de estoy aprendiendo git

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hola mundo");
+        System.out.println("Estoy aprendiendo Git");
     }
 }
