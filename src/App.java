07856da24f46594ed591f11e6fb49a5b587a9ba6
Marcos <marcos.asensio8@educa.madrid.org>
2025-01-31 08:54:21 +0100

    version 3, antes del checkout

--- src/App.java
+++ src/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hola mundo");
         System.out.println("Estoy aprendiendo Git");
+        System.out.println("Mensaje para comprobar el checkout");
     }
 }
